67091f9dbc8a0af20e39f0f704b01b44972969a9
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2022-07-05 16:20:53 -0300

    [FEATURE] Added the React Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
+import { Routes, Route, Link } from "react-router-dom";
+
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Olá Mundo</h1>
-      </header>
-      <body>
-        <h2>Um título interessante.</h2>
-        <p>Aqui vai um paragrafo interessante.</p>
-        <HomePage />
-      </body>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
